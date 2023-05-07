155a33a1c351f0f0027d37a77ea7941cc8d18b59
theodeeney <theo.paraskeva123@gmail.com>
2023-05-07 19:56:35 +0100

    sw

---
+++ sliding window/209. Minimum Size Subarray Sum.py
@@ -0,0 +1,35 @@
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+# subarray
+#  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+# Example 1:
+
+# Input: target = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+# Example 2:
+
+# Input: target = 4, nums = [1,4,4]
+# Output: 1
+# Example 3:
+
+# Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+# Output: 0
+
+from typing import List
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        c = 0
+        mn = 10**9
+
+        for r in range(len(nums)):
+            c+=nums[r]
+            while c>= target:
+                mn = min(mn,(r-l)+1)
+                c-=nums[l]
+                l+=1
+        return 0 if mn == 10**9 else mn
+
