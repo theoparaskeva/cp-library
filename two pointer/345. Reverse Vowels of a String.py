99796ed5cefcc236d5333261d8f7374590d77d42
theodeeney <theo.paraskeva123@gmail.com>
2023-05-22 14:00:14 +0100

    notes

--- two pointer/345. Reverse Vowels of a String.py
+++ two pointer/345. Reverse Vowels of a String.py
@@ -18,23 +18,23 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         l,r = 0,len(s)-1
         vow = ["a","e","i","o","u","A","E","I","O","U"]
+        ### put in set for quick lookup
         vow = set(vow)
         s = list(s)
         while l<r:
+            ### if both left and right pointer are vowels we switch them, then inc/dec each pointer
             if s[l] in vow and s[r] in vow:
                 s[l],s[r] = s[r],s[l]
                 l+=1
                 r-=1
             
+            ### if our left pointer is not a vowel we increment until it is
             elif s[l] not in vow:
                 l+=1
-            
+            ### if our right pointer is not a vowel we increment until it is
             elif s[r] not in vow:
                 r-=1
             
-            else:
-                l+=1
-                r-=1
         
         return "".join(s)
 
